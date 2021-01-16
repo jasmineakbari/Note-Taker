9588082fd70e819fc94be09b3e7a273e1bf870ee
unknown <jasmineakbari@yahoo.com>
2021-01-16 14:08:56 -0500

    created post request handler and testing post request w/ insomnia

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// how to hand post requests from front end
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+// listens for requests and responses
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
