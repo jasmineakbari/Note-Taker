f7bd52ebb1fccb9b697a27797593ee74aaeb5797
unknown <jasmineakbari@yahoo.com>
2021-01-16 14:04:10 -0500

    corrected syntax error in db.json

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
+const { notes } = require('./db/db');
 
 // retrieves existing notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-})
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
