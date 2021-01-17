6ae6f87b668ccdc6727d1f2893f7d2edf7ccd215
unknown <jasmineakbari@yahoo.com>
2021-01-17 13:41:48 -0500

    corrected html pathing from html routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 
 // html route for landing page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 // html route for Notes page
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 module.exports = router;
